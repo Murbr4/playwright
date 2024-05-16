c1aa9c61520eeb608744c488ff98ad61a147b28e
Murbr4 <murillo.ios02@outlook.com>
2024-05-16 20:44:06 -0300

    subida de step do shooping

---
+++ tests/pages/shopping.ts
@@ -0,0 +1,10 @@
+
+import * as dotenv from 'dotenv';
+import { Given, Then, When } from "@cucumber/cucumber"
+
+ When('o formulario é submetido com os dados', async function (dataTable) {
+ });
+
+
+Then('deverá apresentar mensagem de erro para o campo {string}', async function (string) {
+});
---
+++ tests/step-definition/shopping_step.ts
@@ -0,0 +1,41 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given('que eu acesso a página inicial do Magento', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('que eu acesso a seção de vestuário {string}', function (jaqueta) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('que eu acesso a página de {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('seleciono o primeiro produto da lista', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('seleciono o tamanho {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('seleciono a cor {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('adiciono o produto no carrinho', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('vejo a mensagem {string}', function (mensagem) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
